59cadac5d9da80c925ec7a4c0618b98625e2f82c
KuoHaoZeng <khzeng@cs.washington.edu>
2023-08-25 00:08:25 -0700

    upload

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ except:
 def load_dataset() -> prior.DatasetDict:
     """Load the houses dataset."""
     data = {}
-    for split, size in zip(("train", "val", "test"), (10, 10, 10)):
-        if not f"debug_object_locations_with_feasible_agent_poses_{split}.jsonl.gz" in os.listdir("./"):
-            url = f"https://prior-datasets.s3.us-east-2.amazonaws.com/manipulathor_procthor_10k/debug_object_locations_with_feasible_agent_poses_{split}.jsonl.gz"
+    for split, size in zip(("train", "val", "test"), (9995, 1000, 1000)):
+        if not f"object_locations_with_feasible_agent_poses_{split}.jsonl.gz" in os.listdir("./"):
+            url = f"https://prior-datasets.s3.us-east-2.amazonaws.com/manipulathor_procthor_10k/object_locations_with_feasible_agent_poses_{split}.jsonl.gz"
             urllib.request.urlretrieve(
-                url, "./debug_object_locations_with_feasible_agent_poses_{}.jsonl.gz".format(split)
+                url, "./object_locations_with_feasible_agent_poses_{}.jsonl.gz".format(split)
             )
-        with gzip.open(f"debug_object_locations_with_feasible_agent_poses_{split}.jsonl.gz", "r") as f:
+        with gzip.open(f"object_locations_with_feasible_agent_poses_{split}.jsonl.gz", "r") as f:
             houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
         data[split] = LazyJsonDataset(data=houses, dataset="manipulathor-procthor-10k", split=split)
     return prior.DatasetDict(**data)
